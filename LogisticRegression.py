af443c9c330853b12ec05b7ee63cd8e15fe745ba
ESRA ÖZKURT <100353067+esraozkurt@users.noreply.github.com>
2023-08-15 15:47:26 +0300

    Add files via upload

---
+++ LogisticRegression.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.linear_model import LogisticRegression
+
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+Y = iris.target
+
+# Create an instance of Logistic Regression Classifier and fit the data.
+logreg = LogisticRegression(C=1e5)
+logreg.fit(X, Y)
+
+_, ax = plt.subplots(figsize=(4, 3))
+DecisionBoundaryDisplay.from_estimator(
+    logreg,
+    X,
+    cmap=plt.cm.Paired,
+    ax=ax,
+    response_method="predict",
+    plot_method="pcolormesh",
+    shading="auto",
+    xlabel="Sepal length",
+    ylabel="Sepal width",
+    eps=0.5,
+)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors="k", cmap=plt.cm.Paired)
+
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
